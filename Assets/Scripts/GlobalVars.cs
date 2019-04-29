c004c340fe17426f80edab2cd184a334907adf13
AustinYQM <a.r.mcgowan@gmail.com>
2019-04-29 06:31:29 -0500

    added assets folder

---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVars : Singleton<GlobalVars>
+{
+    
+    protected GlobalVars() { /*This prevent people making GlobalVars Objects, not really needed but nice. */ }
+
+    /* Global Variables, can be used via GlobalVars.name */
+    public static float baseEnemeyHealth = 1f; // Every enemy starts at the same health. Games up over time.
+    public static float baseTimeScore = 10f; // Base points rewarded per second. 
+    public static float baseEnemyScore = 5f; // Base points rewarded per kill
+    public static float baseEnemySpeed = 5f;
+
+    public static float basePlayerSpeed = 5f;
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame updateFacebook showed me a memory from 2 years ago where I was excited to be sub-250. Now I am 355. Ugh.
+
+    private float moveSpeed;
+    private Rigidbody myRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveSpeed = GlobalVars.basePlayerSpeed;
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
